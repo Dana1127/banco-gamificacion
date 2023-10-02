b7258b5d0f59d271edea43b3ca3f808a73fef2fa
Dana1127 <82677569+Dana1127@users.noreply.github.com>
2023-10-02 15:43:40 -0500

    Renombrar los archivos para entender la secuencia

--- scripts/buttonP2.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("boton").addEventListener("click", function() {
-    window.location.href = "pantalla3.html"; 
-});
-
-
---
+++ scripts/buttons.js
@@ -0,0 +1,12 @@
+
+document.getElementById("btn3-1").addEventListener("click", function() {
+    window.location.href = "pantalla4.html"; 
+});
+
+
+document.getElementById("btn3-1").addEventListener("click", function() {
+    window.location.href = "pantalla4.html"; 
+});
+
+
+
